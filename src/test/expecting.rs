148e55cb6db3a19fa825ffad3d9e2e8e1f02b00e
Tommie Gannert <tommie@gannert.se>
2024-09-21 10:33:05 +0200

    Adds a test to ExpectingTokenSink.
    It also makes it default to the underlying expectations. This isn't
    necessarily a good thing, but it's unlikely tests want None where the
    underlying actually has an opinion.

--- src/test/expecting.rs
+++ src/test/expecting.rs
@@ -6,12 +6,19 @@ use maybe_owned::MaybeOwnedMut;
 use crate::token::*;
 use crate::TokenSink;
 
+/// Overrides the [TokenSink::expect_tokens] for another sink.
+///
+/// This can be useful when testing sources that can negotiate token
+/// types, e.g. JSON-encoded enums.
 pub struct ExpectingTokenSink<'a, S: TokenSink, F: Fn(usize) -> Option<TokenTypes>> {
     inner: Rc<RefCell<(usize, F)>>,
     sink: MaybeOwnedMut<'a, S>,
 }
 
 impl<'a, S: TokenSink, F: Fn(usize) -> Option<TokenTypes>> ExpectingTokenSink<'a, S, F> {
+    /// Constructs a new wrapping sink. The function is called with
+    /// the current token count, starting at zero. It is incremented
+    /// for all tokens.
     pub fn new(sink: &'a mut S, fun: F) -> Self {
         Self {
             inner: Rc::new(RefCell::new((0, fun))),
@@ -66,6 +73,42 @@ impl<'a, S: TokenSink, F: Fn(usize) -> Option<TokenTypes>> TokenSink
     fn expect_tokens(&mut self) -> Option<TokenTypes> {
         let inner = self.inner.borrow_mut();
 
-        inner.1(inner.0)
+        inner.1(inner.0).or_else(|| self.sink.expect_tokens())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::vec::*;
+
+    #[test]
+    fn test_first() {
+        let mut got = TokenVec::new();
+        let mut exp = ExpectingTokenSink::new(&mut got, |_| Some(TokenTypes::new(TokenType::U32)));
+        assert_eq!(exp.expect_tokens(), Some(TokenTypes::new(TokenType::U32)));
+    }
+
+    #[test]
+    fn test_second() {
+        let mut got = TokenVec::new();
+        let mut exp = ExpectingTokenSink::new(&mut got, |i| {
+            if i == 1 {
+                Some(TokenTypes::new(TokenType::U32))
+            } else {
+                None
+            }
+        });
+        assert_eq!(exp.expect_tokens(), None);
+        exp.yield_token(Token::Bool(true)).unwrap();
+        assert_eq!(exp.expect_tokens(), Some(TokenTypes::new(TokenType::U32)));
+    }
+
+    #[test]
+    fn test_default() {
+        let mut got = TokenVec::new();
+        let mut exp1 = ExpectingTokenSink::new(&mut got, |_| Some(TokenTypes::new(TokenType::U32)));
+        let mut exp2 = ExpectingTokenSink::new(&mut exp1, |_| None);
+        assert_eq!(exp2.expect_tokens(), Some(TokenTypes::new(TokenType::U32)));
     }
 }
