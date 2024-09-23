075ed627bc4474fa3b05bf843a32d8fb52f7c3d4
Tommie Gannert <tommie@gannert.se>
2024-09-23 15:46:01 +0200

    Adds benchmarks for basic FromTokens.
    Compared to before UTF8Read:
    
    read_match is not good:
    
      bool true               time:   [16.141 ns 16.191 ns 16.256 ns] [+225.97% +229.17% +232.34%]
      bool false              time:   [14.580 ns 14.618 ns 14.660 ns] [+185.23% +188.85% +192.40%]
    
    f64 suffers:
    
      f64 42.5                time:   [26.989 ns 27.066 ns 27.162 ns] [+69.440% +71.255% +73.187%]
    
    Basic values suffer 40-50%:
    
      u8 42                   time:   [23.066 ns 23.106 ns 23.148 ns] [+52.954% +54.134% +55.283%]
      u32 42                  time:   [23.290 ns 23.430 ns 23.625 ns] [+48.576% +49.725% +50.883%]
      u64 42                  time:   [23.206 ns 23.243 ns 23.284 ns] [+34.190% +35.497% +36.823%]
      u128 42                 time:   [24.147 ns 24.366 ns 24.617 ns] [+44.562% +45.845% +47.197%]
      i8 -42                  time:   [26.618 ns 26.929 ns 27.256 ns] [+50.703% +52.193% +53.823%]
      i32 -42                 time:   [26.375 ns 26.461 ns 26.572 ns] [+41.488% +43.255% +45.190%]
      i64 -42                 time:   [26.610 ns 26.984 ns 27.362 ns] [+49.894% +51.893% +54.170%]
      i128 -42                time:   [28.470 ns 28.622 ns 28.813 ns] [+44.146% +46.272% +48.333%]
      Vec<u8> "AA=="          time:   [71.444 ns 71.833 ns 72.297 ns] [+35.147% +36.349% +37.578%]
    
    Characters and strings barely changed:
    
      char "H"                time:   [17.500 ns 17.658 ns 17.838 ns] [+13.793% +15.170% +16.654%]
      String "Hello"          time:   [35.586 ns 36.075 ns 36.561 ns] zero

---
+++ benches/criterion.rs
@@ -0,0 +1,44 @@
+use std::fmt;
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+use serser::json::*;
+use serser::FromTokenSink;
+
+fn bench_basic<T: FromTokenSink + fmt::Debug + PartialEq>(json: &str, want: T) {
+    if cfg!(debug_assertions) {
+        assert_eq!(json_into::<T, _>(json.as_bytes()).unwrap(), want);
+    } else {
+        json_into::<T, _>(json.as_bytes()).unwrap();
+    }
+}
+
+fn criterion_basic(c: &mut Criterion) {
+    macro_rules! basic_json_into (
+        ($ty:ty, $json:literal => $want:expr) => {
+            c.bench_function(concat!(stringify!($ty), " ", $json), |b| b.iter(|| bench_basic::<$ty>(black_box($json), $want)));
+        }
+    );
+
+    basic_json_into!(bool, "true" => true);
+    basic_json_into!(bool, "false" => false);
+
+    basic_json_into!(u8,   "42" => 42);
+    basic_json_into!(u32,  "42" => 42);
+    basic_json_into!(u64,  "42" => 42);
+    basic_json_into!(u128, "42" => 42);
+
+    basic_json_into!(i8,   "-42" => -42);
+    basic_json_into!(i32,  "-42" => -42);
+    basic_json_into!(i64,  "-42" => -42);
+    basic_json_into!(i128, "-42" => -42);
+
+    basic_json_into!(f64, "42.5" => 42.5);
+
+    basic_json_into!(char, r#""H""# => 'H');
+    basic_json_into!(Vec<u8>, r#""AA==""# => b"\x00".to_vec());
+    basic_json_into!(String, r#""Hello""# => "Hello".to_owned());
+}
+
+criterion_group!(benches, criterion_basic);
+criterion_main!(benches);
